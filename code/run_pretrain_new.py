6bdfbc1dcc35109c31a3fee0513f91b72f2b9ef6
Nikit Srivastava <nikit91@gmail.com>
2020-12-02 18:19:26 +0100

    reverting changes

--- code/run_pretrain_new.py
+++ code/run_pretrain_new.py
@@ -248,6 +248,8 @@ def main():
               cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(args.local_rank))
     #if args.fp16:
     #    model.half()
+    model.to(device)
+
 
     # Prepare optimizer
     param_optimizer = list(model.named_parameters())
@@ -300,8 +302,6 @@ def main():
         model = DDP(model)
     elif n_gpu > 1:
         model = torch.nn.DataParallel(model)
-
-    model.to(device)
 
     global_step = 0
     if args.do_train:
