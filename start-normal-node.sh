8760fba7515c0c10ace28a9989d304d68f830d1f
Nikit Srivastava <nikit91@gmail.com>
2020-12-28 21:53:01 +0100

    fixing multiple line command

--- start-normal-node.sh
+++ start-normal-node.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Starting normal node: $1"
-python3 -m torch.distributed.launch --nproc_per_node=1
-           --nnodes=8 --node_rank=$1 --master_addr=$MASTER_NODE_ADDR
-           --master_port=8200 code/run_pretrain_dist_util.py --do_train --data_dir sample_ptdata/sample --bert_model ernie_base --output_dir sample_pretrain_out/ --task_name pretrain --fp16 --max_seq_length 256
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=8 --node_rank=$1 --master_addr=$MASTER_NODE_ADDR --master_port=8200 code/run_pretrain_dist_util.py --do_train --data_dir sample_ptdata/sample --bert_model ernie_base --output_dir sample_pretrain_out/ --task_name pretrain --fp16 --max_seq_length 256
