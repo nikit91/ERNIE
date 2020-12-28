8606f7ac247971362dfb17a569dc0646b3b7cfb9
Nikit Srivastava <nikit91@gmail.com>
2020-12-29 00:53:35 +0100

    reverting to hostname

--- run-normal-nodes.sh
+++ run-normal-nodes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting normal nodes"
 #MASTER_NODE_ADDR=$1
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 1
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 2
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 3
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 4
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 5
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 6
-ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 7
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 1 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 2 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 3 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 4 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 5 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 6 $MASTER_NODE_ADDR
+ccsalloc --duration 2h --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh 7 $MASTER_NODE_ADDR
 echo "Done! Please, check ccsinfo -s | grep <user_name> for allocations and resource usage."
--- start-master-node.sh
+++ start-master-node.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #print the host name
-MASTER_IP=$(hostname -i)
-echo $MASTER_IP
+echo $CCS_NODES
 #run the python script
-python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=8 --node_rank=0 --master_addr=$MASTER_IP --master_port=8200 code/run_pretrain_dist_util.py --do_train --data_dir sample_ptdata/sample --bert_model ernie_base --output_dir sample_pretrain_out/ --task_name pretrain --fp16 --max_seq_length 256
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=8 --node_rank=0 --master_addr=$CCS_NODES --master_port=8200 code/run_pretrain_dist_util.py --do_train --data_dir sample_ptdata/sample --bert_model ernie_base --output_dir sample_pretrain_out/ --task_name pretrain --fp16 --max_seq_length 256
