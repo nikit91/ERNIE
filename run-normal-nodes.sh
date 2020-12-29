68537cfa3acfd5d60da2eaf77e34b83cf039446c
Nikit Srivastava <nikit91@gmail.com>
2020-12-29 11:47:57 +0100

    fixing for loop

--- run-normal-nodes.sh
+++ run-normal-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting normal nodes"
-for g_rank in {1..$3}
+for g_rank in $(seq 1 $3)
 do
 ccsalloc --duration $1 --res=rset=1:ncpus=4:mem=30g:gpus=1:rtx2080=t start-normal-node.sh $g_rank $2 $3 $4 $5
 done
