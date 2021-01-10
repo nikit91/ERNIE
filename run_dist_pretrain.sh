d61a888592a319b19e018cc4e6f50ab8a718b3a3
Nikit Srivastava <nikit91@gmail.com>
2021-01-10 23:44:31 +0100

    reducing block time

--- run_dist_pretrain.sh
+++ run_dist_pretrain.sh
@@ -12,8 +12,8 @@ PRETRAIN_FILE=$4
 #run master node
 ./run-master-node.sh 3d $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE
 #wait for output to be written
-echo "Blocking for 100 seconds"
-sleep 100
+echo "Blocking for 10 seconds"
+sleep 10
 #assign master node address
 MASTER_NODE_ADDR="$(head -n 1 start-master-node.sh.*.out)"
 echo "Master node deployed at: $MASTER_NODE_ADDR"
