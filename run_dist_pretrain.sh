281240541ec2c6ad2cb841a74bcfc92825780fc6
Nikit Srivastava <nikit91@gmail.com>
2021-02-01 12:14:23 +0100

    modifying normal node deploy logic

--- run_dist_pretrain.sh
+++ run_dist_pretrain.sh
@@ -15,11 +15,15 @@ LIM_FLAG=$6
 ./clear_output.sh $OUT_DIR
 #run master node
 ./run-master-node.sh 4d $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
+#Ask for process id
 #wait for output to be written
-echo "Blocking for 10 seconds"
-sleep 10
+read MASTER_PROC_ID
+MASTER_PROC_LOG_FILE="start-master-node.sh.${MASTER_PROC_ID}.out"
+echo "Expected master proc log file name: ${MASTER_PROC_LOG_FILE}"
+#echo "Blocking for 10 seconds"
+#sleep 10
 #assign master node address
-MASTER_NODE_ADDR="$(head -n 1 start-master-node.sh.*.out)"
-echo "Master node deployed at: $MASTER_NODE_ADDR"
+#MASTER_NODE_ADDR="$(head -n 1 start-master-node.sh.*.out)"
+#echo "Master node deployed at: $MASTER_NODE_ADDR"
 #start normal nodes
-./run-normal-nodes.sh 4d $MASTER_NODE_ADDR $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
+./run-normal-nodes.sh 4d $MASTER_PROC_LOG_FILE $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
--- start-normal-node.sh
+++ start-normal-node.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 NCCL_DEBUG=INFO
-echo "Starting normal node: $1 -- Master node address: $2 -- Local address $CCS_NODES -- File name $6"
-python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=$3 --node_rank=$1 --master_addr=$2 --master_port=8286 $6 --do_train --data_dir $4 --bert_model ernie_base --output_dir $5 --task_name pretrain --fp16 --max_seq_length 256 --vec_file $7 --use_lim_ents $8
+FILE_NAME=$2
+MASTER_NODE_ADDR=""
+#wait for master proc log file to be created
+for (( ; ; ))
+do
+  echo "Normal node $1 checking for Master proc log file $FILE_NAME"
+  if [ -f "$FILE_NAME" ]; then
+    echo "$FILE_NAME found."
+    sleep 10
+    MASTER_NODE_ADDR="$(head -n 1 ${FILE_NAME})"
+    break
+  else
+    echo "$FILE_NAME not yet created. Checking again in 10 secs."
+    sleep 10
+  fi
+done
+echo "Starting normal node: $1 -- Master node address: $MASTER_NODE_ADDR -- Local address $CCS_NODES -- File name $6"
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=$3 --node_rank=$1 --master_addr=$MASTER_NODE_ADDR --master_port=8286 $6 --do_train --data_dir $4 --bert_model ernie_base --output_dir $5 --task_name pretrain --fp16 --max_seq_length 256 --vec_file $7 --use_lim_ents $8
