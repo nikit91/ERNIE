378931a5a1442f7019e743dfb4169ec0ee041bf4
Nikit Srivastava <nikit91@gmail.com>
2021-02-01 02:09:43 +0100

    increasing training time limit

--- run_dist_pretrain.sh
+++ run_dist_pretrain.sh
@@ -14,7 +14,7 @@ LIM_FLAG=$6
 #clear previous outputs
 ./clear_output.sh $OUT_DIR
 #run master node
-./run-master-node.sh 2d $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
+./run-master-node.sh 4d $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
 #wait for output to be written
 echo "Blocking for 10 seconds"
 sleep 10
@@ -22,4 +22,4 @@ sleep 10
 MASTER_NODE_ADDR="$(head -n 1 start-master-node.sh.*.out)"
 echo "Master node deployed at: $MASTER_NODE_ADDR"
 #start normal nodes
-./run-normal-nodes.sh 2d $MASTER_NODE_ADDR $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
+./run-normal-nodes.sh 4d $MASTER_NODE_ADDR $WORLD_N $TRAINING_DATA $OUT_DIR $PRETRAIN_FILE $EMBED_FILE $LIM_FLAG
